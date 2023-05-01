81c708637754bf3739ae18afe9a226823cb2d30a
Dmitriy <dm.hmara@gmail.com>
2023-05-01 13:14:51 +0300

    form

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,19 +1,15 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { cars } from './cars.data';
 import CarItem from './car-item/CarItem';
+import CreateCarForm from './create-car-form/CreateCarForm';
 
 const Home = () => {
-
-  const filteredCars = useMemo(
-    () => cars.filter(car => car.price > 24000), []
-  )
-
-
   return (
     <div>
       <h1>Cars catalog</h1>
+      <CreateCarForm />
       <div>
-        {filteredCars.length ? filteredCars.map(car => (
+        {cars.length ? cars.map(car => (
           <CarItem key={car.id} car={car} />
         ))
           : <p>cars not difaned</p>}
